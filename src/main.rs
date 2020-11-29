9e51e5220deb3790780a61d8316bb2930a383465
ragnar <ragnar@rmgk.de>
2020-11-29 23:49:58 +0100

    feature complete (?) viewing

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use std::process::Command;
 use rusqlite::{Connection, NO_PARAMS, Result, ToSql, Transaction};
 use serde_derive::{Deserialize, Serialize};
 
-use crate::structs::{Collection, WallpaperPath, Purity};
+use crate::structs::{Collection, Purity, WallpaperPath, WallpaperInfo};
 
 mod import;
 mod structs;
@@ -21,11 +21,14 @@ pub struct Config {
     current: Option<String>,
     wp_path: String,
     simulate: Option<bool>,
+    collections: Vec<Collection>,
+    random: Vec<Collection>,
 }
 
 
 fn main() -> Result<()> {
-    let mut config: Config = toml::from_str(fs::read_to_string("config.ini").expect("read config").as_str()).expect("parse config");
+    let config_path = "config.toml";
+    let mut config: Config = toml::from_str(fs::read_to_string(config_path).expect("read config").as_str()).expect("parse config");
 
 
     let mut conn = Connection::open(&config.db_path)?;
@@ -34,17 +37,29 @@ fn main() -> Result<()> {
     for arg in args {
         match arg.as_str() {
             "import" => import::import(&tx)?,
-            "rand" => set_wallpaper(select_random(&tx)?, &mut config),
-            "reorder" => reorder(&tx)?,
-            "#trash" => set_collection(Collection::Trash, &config, &tx)?,
-            "#fav" => set_collection(Collection::Favorite, &config, &tx)?,
-            "#shelve" => set_collection(Collection::Shelf, &config, &tx)?,
-            "#display" => set_collection(Collection::Display, &config, &tx)?,
-            "#normal" => set_collection(Collection::Favorite, &config, &tx)?,
-            "#sketchy" => set_purity(Purity::Sketchy, &config, &tx)?,
-            "#nsfw" => set_purity(Purity::NSFW, &config, &tx)?,
-            "#pure" => set_purity(Purity::Normal, &config, &tx)?,
-
+            "rand" => set_wallpaper(select_random(&tx, &config)?, &mut config),
+            "reorder" => {
+                reorder(&tx, &config)?;
+                config.position = Some(1);
+            }
+            "+trash" => set_collection(Collection::Trash, &config, &tx)?,
+            "+fav" => set_collection(Collection::Favorite, &config, &tx)?,
+            "+shelve" => set_collection(Collection::Shelf, &config, &tx)?,
+            "+display" => set_collection(Collection::Display, &config, &tx)?,
+            "+normal" => set_collection(Collection::Favorite, &config, &tx)?,
+            "+sketchy" => set_purity(Purity::Sketchy, &config, &tx)?,
+            "+nsfw" => set_purity(Purity::NSFW, &config, &tx)?,
+            "+pure" => set_purity(Purity::Normal, &config, &tx)?,
+            "info" => {
+                config.current.as_ref().map(|c| select_sha(&c, &tx).map(|w| {
+                    println!("{:?}", w.1)
+                }));
+            }
+            "path" => {
+                config.current.as_ref().map(|c| select_sha(&c, &tx).map(|w| {
+                    println!("{}{}", config.wp_path, w.0.path)
+                }));
+            }
             other => {
                 match other.parse::<i32>() {
                     Ok(mov) => {
@@ -57,7 +72,7 @@ fn main() -> Result<()> {
         }
     }
     tx.commit()?;
-    fs::write("config.ini", toml::to_string(&config).expect("serialize config")).expect("write config");
+    fs::write(config_path, toml::to_string(&config).expect("serialize config")).expect("write config");
     Ok(())
 }
 
@@ -70,18 +85,30 @@ fn set_purity(purity: Purity, config: &Config, tx: &Transaction) -> Result<()> {
 }
 
 
-fn select_random(tx: &Transaction) -> Result<WallpaperPath> {
-    tx.query_row("select path, sha1 from info natural join files where collection = ? order by RANDOM() LIMIT 1", &[Collection::Display], |row| { Ok(WallpaperPath { path: row.get(0)?, sha1: row.get(1)? }) })
+fn select_random(tx: &Transaction, config: &Config) -> Result<WallpaperPath> {
+    let rand = &config.random;
+    let holes: Vec<&str> = rand.iter().map(|_| "?").collect();
+    let sql = format!("select path, sha1 from info natural join files where collection in ({}) order by RANDOM() LIMIT 1", holes.join(", "));
+    tx.query_row(&sql, rand, |row| { Ok(WallpaperPath { path: row.get(0)?, sha1: row.get(1)? }) })
 }
 
 fn select_position(pos: i32, tx: &Transaction) -> Result<WallpaperPath> {
     tx.query_row("select path, sha1 from ordering natural join files where position = ?", &[pos], |row| Ok(WallpaperPath { path: row.get(0)?, sha1: row.get(1)? }))
 }
 
-fn reorder(tx: &Transaction) -> Result<()> {
+fn select_sha(sha1: &str, tx: &Transaction) -> Result<(WallpaperPath, WallpaperInfo)> {
+    let wpp = tx.query_row("select path, sha1 from files where sha1 = ?", &[sha1], |row| Ok(WallpaperPath { path: row.get(0)?, sha1: row.get(1)? }))?;
+    let wpi = tx.query_row("select sha1, collection, purity from info where sha1 = ?", &[sha1], |row| Ok(WallpaperInfo { sha1: row.get(0)?, collection: row.get(1)?, purity: row.get(2)? }))?;
+    Ok((wpp, wpi))
+}
+
+fn reorder(tx: &Transaction, config: &Config) -> Result<()> {
     tx.execute("drop table if exists ordering;", NO_PARAMS)?;
     tx.execute("create table ordering (position INTEGER PRIMARY KEY AUTOINCREMENT, sha1 UNIQUE NOT NULL);", NO_PARAMS)?;
-    tx.execute("insert into ordering (sha1) select sha1 from info where collection = 'Display' order by random(); ", NO_PARAMS)?;
+    let collections = &config.collections;
+    let holes: Vec<&str> = collections.iter().map(|_| "?").collect();
+    let sql = format!("insert into ordering (sha1) select sha1 from info where collection in ({}) order by random(); ", holes.join(", "));
+    tx.execute(&sql, collections)?;
     Ok(())
 }
 
--- src/structs.rs
+++ src/structs.rs
@@ -1,6 +1,8 @@
+use std::str::FromStr;
 
 use rusqlite::{Result, ToSql};
-use rusqlite::types::{ToSqlOutput};
+use rusqlite::types::{FromSql, FromSqlResult, ToSqlOutput, ValueRef, FromSqlError};
+use serde_derive::{Deserialize, Serialize};
 use strum_macros::Display;
 use strum_macros::EnumString;
 
@@ -15,13 +17,25 @@ impl ToSql for Purity {
     fn to_sql(&self) -> Result<ToSqlOutput> { Ok(ToSqlOutput::from(self.to_string())) }
 }
 
-#[derive(Debug, PartialEq, EnumString, Display, FromPrimitive, Copy, Clone)]
+impl FromSql for Purity {
+    fn column_result(value: ValueRef) -> FromSqlResult<Self> {
+        value.as_str().and_then(|s| Purity::from_str(s).map_err(|e| FromSqlError::Other(Box::from(e))))
+    }
+}
+
+#[derive(Debug, PartialEq, EnumString, Display, FromPrimitive, Copy, Clone, Deserialize, Serialize)]
 pub enum Collection {
     Display = 2,
     Favorite = 1,
     Normal = 0,
     Shelf = -1,
     Trash = -2,
+}
+
+impl FromSql for Collection {
+    fn column_result(value: ValueRef) -> FromSqlResult<Self> {
+        value.as_str().and_then(|s| Collection::from_str(s).map_err(|e| FromSqlError::Other(Box::from(e))))
+    }
 }
 
 impl ToSql for Collection {
