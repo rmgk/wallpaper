67d8b03184e59d7e131429c53a3635a5f28e3326
ragnar <ragnar@rmgk.de>
2020-12-07 15:47:46 +0100

    restructure config usage

--- v2/src/main.rs
+++ v2/src/main.rs
@@ -18,27 +18,27 @@ mod structs;
 
 #[derive(Deserialize, Serialize)]
 pub struct Config {
-    db_path: String,
-    position: Option<i32>,
-    current: Option<String>,
-    wp_path: String,
-    simulate: Option<bool>,
-    ordered: Vec<Collection>,
-    random: Vec<Collection>,
-    order_filter: Vec<Purity>,
-    random_filter: Vec<Purity>,
+    database_path: String,
+    wallpaper_path: String,
+    order_collections: Vec<Collection>,
+    random_collections: Vec<Collection>,
+    order_purity: Vec<Purity>,
+    random_purity: Vec<Purity>,
 }
 
 fn main() -> Result<()> {
-    let config_path = "config.toml";
-    let mut config: Config = toml::from_str(
-        fs::read_to_string(config_path)
-            .expect("read config")
+    let xdg_dirs = xdg::BaseDirectories::with_prefix("wpc").unwrap();
+    let config_path = xdg_dirs.place_config_file("wpc.toml").expect("could not create config directory");
+
+    let config: Config = toml::from_str(
+        fs::read_to_string("wpc.toml").or_else(
+            |_error| fs::read_to_string(&config_path))
+            .expect(&format!("no config file in »{:?}«", config_path))
             .as_str(),
     )
         .expect("parse config");
 
-    let mut conn = Connection::open(&config.db_path)?;
+    let mut conn = Connection::open(&config.database_path)?;
 
     let max_pos = conn
         .query_row("select max(position) from ordering", NO_PARAMS, |row| {
@@ -57,39 +57,40 @@ fn main() -> Result<()> {
                 "initialize-database" => create_tables(&tx)?,
                 "import-database" => import::import(&tx)?,
                 "scan" => scan_new_files(&config, &tx)?,
-                "rand" => set_wallpaper(select_random(&tx, &config)?, &mut config),
+                "rand" => set_wallpaper(select_random(&tx, &config)?, &config, &tx)?,
                 "reorder" => {
                     reorder(&tx, &config)?;
-                    config.position = Some(1);
+
+                    set_position(1, &tx)?;
                 }
-                "+trash" => set_collection(Collection::Trash, &config, &tx)?,
-                "+fav" => set_collection(Collection::Favorite, &config, &tx)?,
-                "+shelve" => set_collection(Collection::Shelf, &config, &tx)?,
-                "+display" => set_collection(Collection::Display, &config, &tx)?,
-                "+normal" => set_collection(Collection::Favorite, &config, &tx)?,
-                "+sketchy" => set_purity(Purity::Sketchy, &config, &tx)?,
-                "+nsfw" => set_purity(Purity::NSFW, &config, &tx)?,
-                "+pure" => set_purity(Purity::Pure, &config, &tx)?,
+                "+trash" => set_collection(Collection::Trash, &tx)?,
+                "+fav" => set_collection(Collection::Favorite, &tx)?,
+                "+shelve" => set_collection(Collection::Shelf, &tx)?,
+                "+display" => set_collection(Collection::Display, &tx)?,
+                "+normal" => set_collection(Collection::Favorite, &tx)?,
+                "+sketchy" => set_purity(Purity::Sketchy, &tx)?,
+                "+nsfw" => set_purity(Purity::NSFW, &tx)?,
+                "+pure" => set_purity(Purity::Pure, &tx)?,
                 "info" => {
-                    config
-                        .current
-                        .as_ref()
-                        .map(|c| select_sha(&c, &tx).map(|w| println!("{:?}", w.1)));
+                    let current = get_current(&tx)?;
+                    select_sha(&current, &tx).map(|(wpp, wpi)| {
+                        println!("{:?}\n{}", wpi, wpp.path)
+                    })?;
                 }
                 "path" => {
-                    config.current.as_ref().map(|c| {
-                        select_sha(&c, &tx).map(|w| println!("{}{}", config.wp_path, w.0.path))
-                    });
+                    let current = get_current(&tx)?;
+                    select_sha(&current, &tx).map(|w| println!("{}{}", config.wallpaper_path, w.0.path))?;
                 }
                 other => match other.parse::<i32>() {
                     Ok(mov) => {
-                        let next_pos = config.position.unwrap_or(1) + mov;
+                        let next_pos = get_position(&tx).unwrap_or(1) + mov;
                         if next_pos <= max_pos && next_pos >= 1 {
-                            config.position = Some(next_pos);
+                            set_position(next_pos, &tx)?;
                             set_wallpaper(
-                                select_position(config.position.unwrap_or(1), &tx)?,
-                                &mut config,
-                            );
+                                select_current_position(&tx)?,
+                                &config,
+                                &tx,
+                            )?;
                         }
                     }
                     Err(_) => {
@@ -107,6 +108,22 @@ fn main() -> Result<()> {
     )
         .expect("write config");
     Ok(())
+}
+
+fn set_position(pos: i32, tx: &Transaction) -> Result<usize> {
+    tx.execute("replace into settings (key, value) values ('position', ?)", &[pos])
+}
+
+fn set_current(sha1: &str, tx: &Transaction) -> Result<usize> {
+    tx.execute("replace into settings (key, value) values ('current', ?)", &[sha1])
+}
+
+fn get_position(tx: &Transaction) -> Result<i32> {
+    tx.query_row("select value from settings where key = 'position'", NO_PARAMS, |row| row.get(0))
+}
+
+fn get_current(tx: &Transaction) -> Result<String> {
+    tx.query_row("select value from settings where key ='current'", NO_PARAMS, |row| row.get(0))
 }
 
 fn help() {
@@ -145,6 +162,11 @@ fn create_tables(tx: &Transaction) -> Result<()> {
         "create index idx_info_collection on info (collection);",
         NO_PARAMS,
     )?;
+    tx.execute(
+        "create table settings (key TEXT PRIMARY KEY, value TEXT NOT NULL);",
+        NO_PARAMS,
+    )?;
+
     Ok(())
 }
 
@@ -168,7 +190,7 @@ fn scan_new_files(config: &Config, tx: &Transaction) -> Result<()> {
     let mut insert_info_stmt =
         tx.prepare("insert or ignore into info (sha1, collection, purity) values (?, ?, ?)")?;
 
-    WalkDir::new(&config.wp_path)
+    WalkDir::new(&config.wallpaper_path)
         .follow_links(true)
         .into_iter()
         .filter_map(|e| e.ok())
@@ -182,7 +204,7 @@ fn scan_new_files(config: &Config, tx: &Transaction) -> Result<()> {
             path: e.clone(),
             relative: String::from(
                 e.path()
-                    .strip_prefix(&config.wp_path)
+                    .strip_prefix(&config.wallpaper_path)
                     .expect("strip prefix")
                     .to_str()
                     .expect("to str"),
@@ -232,25 +254,27 @@ fn scan_new_files(config: &Config, tx: &Transaction) -> Result<()> {
     Ok(())
 }
 
-fn set_collection(collection: Collection, config: &Config, tx: &Transaction) -> Result<()> {
+fn set_collection(collection: Collection, tx: &Transaction) -> Result<()> {
+    let current_sha = get_current(&tx)?;
     tx.execute::<&[&dyn ToSql]>(
         "update info set collection = ? where sha1 = ?",
-        &[&collection, &config.current],
+        &[&collection, &current_sha],
     )
         .map(|_| ())
 }
 
-fn set_purity(purity: Purity, config: &Config, tx: &Transaction) -> Result<()> {
+fn set_purity(purity: Purity, tx: &Transaction) -> Result<()> {
+    let current_sha = get_current(&tx)?;
     tx.execute::<&[&dyn ToSql]>(
         "update info set purity = ? where sha1 = ?",
-        &[&purity, &config.current],
+        &[&purity, &current_sha],
     )
         .map(|_| ())
 }
 
 fn select_random(tx: &Transaction, config: &Config) -> Result<WallpaperPath> {
-    let rand = &config.random;
-    let filter = &config.random_filter;
+    let rand = &config.random_collections;
+    let filter = &config.random_purity;
     let sql =
         format!("select path, sha1 from info natural join files where collection in ({}) and purity in ({}) order by RANDOM() LIMIT 1",
                 qstr(rand.len()),
@@ -263,10 +287,10 @@ fn select_random(tx: &Transaction, config: &Config) -> Result<WallpaperPath> {
     })
 }
 
-fn select_position(pos: i32, tx: &Transaction) -> Result<WallpaperPath> {
+fn select_current_position(tx: &Transaction) -> Result<WallpaperPath> {
     tx.query_row(
-        "select path, sha1 from ordering natural join files where position = ?",
-        &[pos],
+        "select path, sha1 from ordering natural join files, settings where position = settings.value and settings.key = 'position'",
+        NO_PARAMS,
         |row| {
             Ok(WallpaperPath {
                 path: row.get(0)?,
@@ -313,17 +337,16 @@ fn sconcat<'a>(col: &'a Vec<Collection>, pur: &'a Vec<Purity>) -> Vec<&'a dyn To
 }
 
 fn reorder(tx: &Transaction, config: &Config) -> Result<()> {
-    config.position.map(|p| {
-        tx.execute::<&[&dyn ToSql]>("update or ignore info set collection = ? from (select sha1 from ordering where position < ?) as seen where collection = ? and info.sha1 = seen.sha1", &[&Collection::Normal, &p, &Collection::New]).unwrap();
-    });
+    tx.execute::<&[&dyn ToSql]>("update or ignore info set collection = ? from (select sha1 from ordering, settings where position < settings.value and settings.key = 'position') as seen where collection = ? and info.sha1 = seen.sha1", &[&Collection::Normal, &Collection::New]).unwrap();
+
     tx.execute("drop table if exists ordering;", NO_PARAMS)?;
     tx.execute(
-        "create table ordering (position INTEGER PRIMARY KEY AUTOINCREMENT, sha1 UNIQUE NOT NULL);",
+        "create table ordering (position INTEGER PRIMARY KEY AUTOINCREMENT, sha1 TEXT UNIQUE NOT NULL);",
         NO_PARAMS,
     )?;
 
-    let collections = &config.ordered;
-    let filter = &config.order_filter;
+    let collections = &config.order_collections;
+    let filter = &config.order_purity;
 
     let sql = format!("insert into ordering (sha1) select sha1 from info where collection in ({}) and purity in ({}) order by random(); ",
                       qstr(collections.len()),
@@ -333,19 +356,17 @@ fn reorder(tx: &Transaction, config: &Config) -> Result<()> {
     Ok(())
 }
 
-fn set_wallpaper(wpi: WallpaperPath, config: &mut Config) {
-    let full = format!("{}{}", config.wp_path, wpi.path);
+fn set_wallpaper(wpi: WallpaperPath, config: &Config, tx: &Transaction) -> Result<()> {
+    let full = format!("{}{}", config.wallpaper_path, wpi.path);
     let size = imagesize::size(&full).expect("parse image size");
-    config.current = Some(wpi.sha1);
 
-    if !config.simulate.unwrap_or(false) {
-        let status = Command::new("set-wallpaper")
-            .args(&[full, size.width.to_string(), size.height.to_string()])
-            .status()
-            .expect("failed to execute process");
-        assert!(status.success(), "setting wallpaper command failed")
-    } else {
-        println!("set »{}«", full);
-    }
+    set_current(&wpi.sha1, tx)?;
+
+    let status = Command::new("set-wallpaper")
+        .args(&[full, size.width.to_string(), size.height.to_string()])
+        .status()
+        .expect("failed to execute process");
+    assert!(status.success(), "setting wallpaper command failed");
+    Ok(())
 }
 
